5f4f8aacea7efde16ed0ae0b01f871eb72da63c4
sammysam <arin.skysong@gmail.com>
2014-05-10 15:50:40 -0700

    added events that update tip and total when bill or the percentage segments are updated

--- TipCalculator/TipViewController.m
+++ TipCalculator/TipViewController.m
@@ -9,7 +9,13 @@
 #import "TipViewController.h"
 
 @interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountUIField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountUIField;
+@property (weak, nonatomic) IBOutlet UILabel *totalAmountUIField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercentageSegments;
 
+- (IBAction)tapOut:(id)sender;
+- (void)updateValues;
 @end
 
 @implementation TipViewController
@@ -28,6 +34,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self updateValues];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,4 +44,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)tapOut:(id)sender {
+    [self.view endEditing:YES]; //sends keyboard away
+    [self updateValues];
+}
+
+- (void)updateValues {
+    float billValue = [self.billAmountUIField.text floatValue];
+    
+    NSArray *tipPercentages = @[@(0.1), @(0.15), @(0.2)];
+    float selectedPercentage = [tipPercentages[self.tipPercentageSegments.selectedSegmentIndex] floatValue];
+    
+    float tipValue = billValue * selectedPercentage;
+    float totalValue = billValue + tipValue;
+    
+    self.tipAmountUIField.text = [NSString stringWithFormat:@"$%.2f", tipValue];
+    self.totalAmountUIField.text = [NSString stringWithFormat:@"$%.2f", totalValue];
+}
 @end
